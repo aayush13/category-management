06a6bab0d90089744b073bde9d8fd0edcefdc055
Aayush Nagpal <aayushnagpal13@gmail.com>
2024-11-28 03:39:48 +0530

    cors issue

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const categoryRoutes = require('./routes/categoryRoutes');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
